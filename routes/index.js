dd474448179466f5fff35f3a4e37d29fe3a8be1b
salgadodavid <salgado_d@outlook.com>
2022-09-18 19:46:35 -0400

    added mvc structure, middleware, homepage

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/auth') 
+const homeController = require('../controllers/index')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+router.get('/', homeController.getIndex)
+router.get('/login', authController.getLogin)
+router.post('/login', authController.postLogin)
+router.get('/logout', authController.logout)
+router.get('/signup', authController.getSignup)
+router.post('/signup', authController.postSignup)
