73d345d84f44be8299a44f37311c5125a06f3aab
salgadodavid <salgado_d@outlook.com>
2022-10-04 17:12:57 -0400

    removed unnecesary files

--- controllers/comments.js
+++ controllers/comments.js
@@ -17,13 +17,3 @@ module.exports = {
     }
   }
 }
-
-// ,
-//     getComments: async (req, res) => {
-//         try {
-//           const comments = await Comment.find().sort({ createdAt: "desc" }).lean();  // , { posts: posts }
-//           res.render("feed.ejs");  
-//         } catch (err) {
-//           console.log(err);
-//         }
-//       },
