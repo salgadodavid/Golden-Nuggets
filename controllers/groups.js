9ca8ea995732c7bdffda9cdd1479921b08346d23
salgadodavid <salgado_d@outlook.com>
2022-10-18 21:55:29 -0400

    removed unnecesary document from partials

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,6 +1,28 @@
 const Group = require("../models/Group");
+const Post = require("..models/Post")
+const User = require("..models/User")
 
 module.exports = {
+  getGroupPage: async (req, res) => {
+    try {
+      const posts = await Post.find({ bookTitle: req.params.bookTitle }).lean();
+      const userInfo = await User.find({googleId: req.user.googleId})
+      res.render("group.ejs", {posts: posts});
+    } catch (err) {
+      console.log(err);
+    }
+  }, 
+  joinGroup: async (req, res) => {
+    try {
+      await Group.updateOne({bookTitle: req.params.bookTitle
+      
+      });
+      console.log("Group has been createdd!");
+      res.redirect("/feed");  //redirected to profile
+    } catch (err) {
+      console.log(err);
+    }
+  }
   newGroupForm: (req, res) => {
       res.render("newGroup.ejs");
   },
@@ -12,7 +34,6 @@ module.exports = {
         bookTitle: req.body.bookTitle.toLowerCase(),
         author: req.body.author,
         description: req.body.description,
-        
       });
       console.log("Group has been createdd!");
       res.redirect("/feed");  //redirected to profile
--- routes/group.js
+++ routes/group.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const groupsController = require("../controllers/groups");
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-
+router.get("/:bookTitle", ensureAuth, groupsController.getGroupPage);
+router.put("/joinGroup", ensureAuth, groupsController.joinGroup);
+router.post("/createPost", ensureAuth, groupsController.createPost);
 router.get("/createGroup", ensureAuth, groupsController.newGroupForm);
 router.post("/createGroup", ensureAuth, groupsController.createGroup);
 
